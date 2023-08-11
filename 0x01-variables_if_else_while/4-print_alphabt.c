b9f11a783dcbc40f5c636b0d9dfd9b687d68639a
root <root@DESKTOP-74I4R4C.localdomain>
2023-08-11 19:14:06 +0200

    c program that print alphabets without e and q using while loop

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,32 +3,23 @@
 /**
  * main - Entry point
  * Description: This program will print alphabets in lowercase
- *               and then in uppercase
+ *               without 'e' and 'q'
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
 	char alpha = 'a';
-	char low = 'f';
-    char alp = 'r';
-	/*alpha in lowercase*/
-	while (alpha <= 'd')
+
+	/*alpha without 'e' and 'q'*/
+	while (alpha <= 'z')
 	{
+		if (alpha != 'e' && alpha != 'q')
+		{
 		putchar(alpha);
+		}
 		alpha++;
 	}
-	/*alpha in uppercase*/
-	while (low <= 'p')
-	{
-		putchar(low);
-		low++;
-	}
-    while (alp <= 'z')
-    {
-        putchar(alp);
-        alp++;
-    }
 	putchar('\n');
 	return (0);
 
