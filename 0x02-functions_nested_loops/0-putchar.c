829c1f2a5fdde2884fa01fcd826396e09496d686
root <root@DESKTOP-74I4R4C.localdomain>
2023-08-15 18:22:58 +0200

    print _putchar using putchar prototype

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,23 @@
-#include <stdio.h>
+#include"main.h"
 
 /**
  * main - Entry point
- * Description: This program will print _putchar using putchar prototype
+ *
+ * Description: prints _putchar using putchar prototype
+ *
  * Return: Always 0 (Success)
 */
+
 int main(void)
 {
-	char name[] = "_putchar";
+	char str[] = "_putchar";
 	int i;
 
-	for (i = 0; i < 8; i++)
+	for (i = 0; i < 8; ++i)
 	{
-		_putchar(name[i]);
-	    _putchar('\n');
+		_putchar(str[i]);
+		_putchar('\n');
 	}
+
 	return (0);
 }
