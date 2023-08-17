dda1f154dd2fc5ed0f8af8348cd8c207e29feefe
root <root@DESKTOP-74I4R4C.localdomain>
2023-08-17 14:15:55 +0200

    print 10 imes numbers from 0 to 14 using putchar

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+/**
+ * print_most_numbers - print 10 times te numbers from 0 to 14
+ * Return: Always 0 (Success)
+*/
+
+void print_most_numbers(void)
+{
+	int num;
+	int count;
+
+	for (num = 0; num <= 13; num++)
+	{
+		for (count = 0; count <= 13; count++)
+		{
+			if (cout >= 9)
+			{
+				_putchar('1');
+				_putchar(count % 10 + '0');
+			}
+		_putchar('\n');
+		}
+	}
+}
