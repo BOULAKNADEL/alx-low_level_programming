35d6b00711d980b8e6b914826b5c6d41a18032e0
root <root@DESKTOP-74I4R4C.localdomain>
2023-08-17 16:18:02 +0200

    draws a diogonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,21 +7,20 @@
  *
  * Return: void
  */
-
 void print_diagonal(int n)
 {
 	int i, j;
 
 	if (n <= 0)
 		_putchar('\n');
-	for (i = 0; i < n; i++)
+	else
 	{
-		for (j = 0; j < i; j++)
+		for (i = 1; i <= n; ++i)
 		{
-			_putchar(' ');
+			for (j = 1; j <= n; ++j)
+				_putchar(' ');
+			_putchar(92); 
+			_putchar('\n');
 		}
-		_putchar('92');
-		_putchar('\n');
 	}
-
 }
