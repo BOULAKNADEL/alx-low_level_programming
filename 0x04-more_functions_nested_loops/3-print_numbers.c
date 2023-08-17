89e63edf9bac63edeb7db1845c496d2433861e59
root <root@DESKTOP-74I4R4C.localdomain>
2023-08-17 13:13:01 +0200

    print the numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the numbers from 0 to 9
+ *
+ * Return: void
+*/
+void print_numbers(void)
+{
+	int count;
+
+	for (count = 0; count < 10; count++)
+	_putchar(count + '0');
+_putchar('\n');
+}
