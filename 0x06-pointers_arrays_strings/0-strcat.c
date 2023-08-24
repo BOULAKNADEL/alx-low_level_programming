58f6c8e367af08384862c76c1d128140b887b675
root <root@DESKTOP-74I4R4C.localdomain>
2023-08-24 18:03:59 +0200

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,16 +12,16 @@
 char *_strcat(char *dest, char *src)
 {
 
-    /* Move pointer to end of destination string */
-    while (*cp != '\0')
-        dest++;
+	/* Move pointer to end of destination string */
+	while (*cp != '\0')
+		dest++;
 
-    /* Copy characters from source string to end of destination string */
-    while (*src != '\0')
-        *dest++ = *src++;
+	/* Copy characters from source string to end of destination string */
+	while (*src != '\0')
+		*dest++ = *src++;
 
-    /* Add null terminator to end of destination string */
-    *dest = '\0';
+	/* Add null terminator to end of destination string */
+	*dest = '\0';
 
-    return dest;
+	return (dest);
 }
