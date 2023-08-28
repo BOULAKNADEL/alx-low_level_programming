5ec0880798029f24c75fa13fce94d93062f7f74a
root <root@DESKTOP-74I4R4C.localdomain>
2023-08-28 17:41:13 +0200

    memset function

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memset - function fills the first @n bytes of the memory area
+ * pointed to by @s with the constant byte @b
+ *
+ *@s: pointer to the memory area
+ *@b: the number of bytes to be filled
+ *@n: bytes of the memory area pointed to by @s
+ *
+ * Return: a pointer to the memory area s
+*/
+
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int counter = 0;
+
+	for (counter = 0; counter < n; counter++)
+	{
+		s[counter] = b;
+	}
+	return (s);
+}
