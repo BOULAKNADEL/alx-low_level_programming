b110657956e080fb35820c317fd5728246cac7ad
root <root@DESKTOP-74I4R4C.localdomain>
2023-08-28 18:35:18 +0200

    memcpy function

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - function copies @n bytes from memory area
+ * @src to memory area @dest
+ *
+ *@src: pointer to the memory area
+ *@dest: memory area where the n bytes will be copied
+ *@n: bytes of the memory area
+ *
+ * Return: a pointer to the memory area @dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int counter = 0; /** because we have an unsigned value of n */
+
+	for (counter = 0; counter < n; counter++)
+	{
+		s[counter] = dest[counter];
+	}
+	return (dest);
+}
