5b1d9bf13aedc5603c0adb82b54fb6fbd408906c
root <root@DESKTOP-74I4R4C.localdomain>
2023-08-28 21:03:19 +0200

    strspn function

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strspn - a function that gets the length of a prefix substring
+ *
+ * @s: pointer to string input
+ *
+ * @accept: substring prefix to look for
+ *
+ * Return: the number of bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int prefixLength = 0;
+	int stringIndex, acceptIndex;
+
+	for (stringIndex = 0; s[stringIndex] != '\0'; stringIndex++)
+	{
+		if (s[stringIndex] != ' ')
+
+		{  /* Changed 32 to ' ' for better clarity */
+			for (acceptIndex = 0; accept[acceptIndex] != '\0'; acceptIndex++)
+			{
+				if (s[stringIndex] == accept[acceptIndex])
+				{
+					prefixLength++;
+				}
+			}
+		} else
+		{
+			return (prefixLength);
+		}
+	}
+
+	return (prefixLength);
+}
