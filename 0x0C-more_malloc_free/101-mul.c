7875131b6ab1557427d52a2180bd4f72cedbe163
BOULAKNADEL <boulaknadelhamza@gmail.com>
2023-11-02 17:40:12 +0100

    c is fun

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,151 +1,100 @@
-#include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
 
 /**
- * print_string - moves a string one place to the left and prints the string
- * @str: string to move
- * @len: size of string
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
  *
- * Return: void
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-void print_string(char *str, int len)
+int is_digit(char *s)
 {
-	int i, j;
+	int i = 0;
 
-	i = j = 0;
-	while (i < len)
+	while (s[i])
 	{
-		if (str[i] != '0')
-			j = 1;
-		if (j || i == len - 1)
-			_putchar(str[i]);
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
 		i++;
 	}
-
-	_putchar('\n');
-	free(str);
+	return (1);
 }
 
 /**
- * multiply - multiplies a char with a string and places the answer into dest
- * @digit: char to multiply
- * @num: string to multiply
- * @num_len: last non NULL index of num
- * @dest: destination of multiplication
- * @dest_len: highest index to start addition
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
  *
- * Return: pointer to dest, or NULL on failure
+ * Return: the length of the string
  */
-char *multiply(char digit, char *num, int num_len, char *dest, int dest_len)
+int _strlen(char *s)
 {
-	int j, k, prod, prod_carry, sum, sum_carry;
+	int i = 0;
 
-	prod_carry = sum_carry = 0;
-	for (j = num_len, k = dest_len; j >= 0; j--, k--)
+	while (s[i] != '\0')
 	{
-		prod = (digit - '0') * (num[j] - '0') + prod_carry;
-		prod_carry = prod / 10;
-		sum = (dest[k] - '0') + (prod % 10) + sum_carry;
-		sum_carry = sum / 10;
-		dest[k] = sum % 10 + '0';
+		i++;
 	}
-	for (sum_carry += prod_carry; k >= 0 && sum_carry; k--)
-	{
-		sum = (dest[k] - '0') + sum_carry;
-		sum_carry = sum / 10;
-		dest[k] = sum % 10 + '0';
-	}
-	if (sum_carry)
-	{
-		return (NULL);
-	}
-	return (dest);
+	return (i);
 }
 
 /**
- * check_digits - checks the arguments to ensure they are digits
- * @av: pointer to arguments
- *
- * Return: 0 if digits, 1 if not
+ * errors - handles errors for main
  */
-int check_digits(char **av)
+void errors(void)
 {
-	int i, j;
-
-	for (i = 1; i < 3; i++)
-	{
-		for (j = 0; av[i][j]; j++)
-		{
-			if (av[i][j] < '0' || av[i][j] > '9')
-				return (1);
-		}
-	}
-	return (0);
+	printf("Error\n");
+	exit(98);
 }
 
 /**
- * initialize - initializes a string
- * @str: sting to initialize
- * @l: length of strinf
- *
- * Return: void
- */
-void initialize(char *str, int l)
-{
-	int i;
-
-	for (i = 0; i < l; i++)
-		str[i] = '0';
-	str[i] = '\0';
-}
-
-/**
- * main - multiply two numbers
+ * main - multiplies two positive numbers
  * @argc: number of arguments
- * @argv: argument vector
+ * @argv: array of arguments
  *
- * Return: zero, or exit status of 98 if failure
+ * Return: always 0 (Success)
  */
-
 int main(int argc, char *argv[])
 {
-	int len1, len2, total_len, temp_index, i;
-	char *answer;
-	char *temp;
-	char err[] = "Error\n";
+	char *num1, *num2;
+	int len_num1, len_num2, total_len, i, carry_over, n1, n2, *product, flag = 0;
 
-	if (argc != 3 || check_digits(argv))
+	num1 = argv[1], num2 = argv[2];
+	if (argc != 3 || !is_digit(num1) || !is_digit(num2))
+		errors();
+	len_num1 = _strlen(num1);
+	len_num2 = _strlen(num2);
+	total_len = len_num1 + len_num2 + 1;
+	product = malloc(sizeof(int) * total_len);
+	if (!product)
+		return (1);
+	for (i = 0; i <= len_num1 + len_num2; i++)
+		product[i] = 0;
+	for (len_num1 = len_num1 - 1; len_num1 >= 0; len_num1--)
 	{
-		for (temp_index = 0; err[temp_index]; temp_index++)
-			_putchar(err[temp_index]);
-		exit(98);
-	}
-	for (len1 = 0; argv[1][len1]; len1++)
-		;
-	for (len2 = 0; argv[2][len2]; len2++)
-		;
-	total_len = len1 + len2 + 1;
-	answer = malloc(total_len * sizeof(char));
-	if (answer == NULL)
-	{
-		for (temp_index = 0; err[temp_index]; temp_index++)
-			_putchar(err[temp_index]);
-		exit(98);
-	}
-	initialize(answer, total_len - 1);
-	for (temp_index = len2 - 1, i = 0; temp_index >= 0; temp_index--, i++)
-	{
-		temp_index = (total_len - 2) - i;
-		temp = multiply(argv[2][temp_index], argv[1], len1 - 1, answer, temp_index);
-		if (temp == NULL)
+		n1 = num1[len_num1] - '0';
+		carry_over = 0;
+		for (len_num2 = _strlen(num2) - 1; len_num2 >= 0; len_num2--)
 		{
-			for (temp_index = 0; err[temp_index]; temp_index++)
-				_putchar(err[temp_index]);
-			free(answer);
-			exit(98);
+			n2 = num2[len_num2] - '0';
+			carry_over += product[len_num1 + len_num2 + 1] + (n1 * n2);
+			product[len_num1 + len_num2 + 1] = carry_over % 10;
+			carry_over /= 10;
 		}
+		if (carry_over > 0)
+			product[len_num1 + len_num2 + 1] += carry_over;
 	}
-	print_string(answer, total_len - 1);
+	for (i = 0; i < total_len - 1; i++)
+	{
+		if (product[i])
+			flag = 1;
+		if (flag)
+			_putchar(product[i] + '0');
+	}
+	if (!flag)
+		_putchar('0');
+	_putchar('\n');
+	free(product);
 	return (0);
 }
 
