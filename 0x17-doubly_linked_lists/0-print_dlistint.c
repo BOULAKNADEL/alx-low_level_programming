a902c49c2c160e6ce61b03c137641f7cae6c57f9
BOULAKNADEL <boulaknadelhamza@gmail.com>
2024-03-21 14:43:47 +0000

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,3 +1,4 @@
+
 #include "lists.h"
 #include <stdio.h>
 
@@ -9,15 +10,20 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    	const dlistint_t *current = h;
+	const dlistint_t *current = h;
     	size_t count = 0;
+
+    	if (h == NULL)
+    	{
+        	return 0;
+    	}
 
     	while (current != NULL)
     	{
         	printf("%d\n", current->n);
         	current = current->next;
         	count++;
-	}
+    	}
 
-	return count;
+    	return count;
 }
